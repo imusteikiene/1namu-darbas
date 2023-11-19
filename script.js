a54db35b086add243dbad7e3c21c39f1cac8b7dd
imusteikiene <149151782+imusteikiene@users.noreply.github.com>
2023-11-19 13:22:18 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,87 @@
+//pirma uzduotis
+var car = "Mini"
+switch (car) {
+    case "Vw":
+    case "Audi":
+    case "Bentley":
+    case "Bugatti":
+    case "Lamborgini":
+    case "Porsche":
+    console.log("priklauso VW prekes zenklui");
+    break
+    case "Bmw":
+    case "Mini":
+    case "Rolls-Royce":
+        console.log("priklauso BMW prekes zenklui");
+        break
+        default:
+        console.log("sis brandas nepriklauso nei VW Group ,nei BMW Group");
+}
+//antra uzduotis
+
+ var userInput = "obuolys";
+
+ switch(userInput) {
+    case "obuolys":
+    case "aviete":
+    case "braske":
+    case "gervuoge":
+    case "melyne":
+        console.log("tai yra vaisius");
+
+
+    case "morka":
+    case "bulve":
+    case "burokelis":
+    case "cesnakas":
+    case "svogunas":
+ console.log("tai yra darzove");
+}
+
+
+//trecia uzduotis
+
+var weekDay = 4;
+ 
+ switch (weekDay) {
+
+ case 0:
+ weekDay = 'pirmadienis';
+ break
+
+ case 1:
+  weekDay = 'antradienis';
+ break
+
+ case 2:
+  weekDay = 'treciadienis';
+ break
+
+ case 3:
+  weekDay = 'ketvirtadienis';
+ break
+
+ case 4:
+  weekDay = 'penktadienis';
+ break
+
+ case 5:
+  weekDay = 'sestadienis';
+ break
+
+
+ case 6:
+  weekDay = 'sekmadienis';
+  break
+
+
+ 
+}
+console.log(weekDay);
+
+
+   
+
+
+
+ 
